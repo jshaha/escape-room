a7a2f3d262d90225ce05d6b129f0b7add6e68ab2
jshaha <shahatitjacob@gmail.com>
2023-12-04 22:24:17 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var teamMembers = {
+    "Team1": ["Alice", "Bob", "Charlie"],
+    "Team2": ["Diana", "Edward", "Fiona"]
+};
+
 function determineFate() {
     var team = document.getElementById('teamSelect').value;
     if (!team) {
@@ -6,12 +11,13 @@ function determineFate() {
     }
 
     var fate = Math.floor(Math.random() * 2) + 1;
+    var members = teamMembers[team].join(", ");
     var resultText = "";
 
     if (fate === 1) {
-        resultText = "Congratulations, " + team + ", You've Escaped!";
+        resultText = "Congratulations to " + members + " of " + team + "! Through wit, wisdom, and unity, you've navigated the treacherous twists of Elsinore. Your astute unraveling of deceit, keen insight into human nature, and courage in the face of uncertainty have won the day. As dawn breaks, you step out of the dungeon's shadows into the light of freedom, leaving the echoes of Elsinore's intrigues behind. Remember, 'To thine own self be true.'"
     } else {
-        resultText = "Alas, " + team + ", You Have Perished in Elsinore.";
+        resultText = "Alas for " + members + " of " + team + ". The labyrinth of Elsinore has claimed another tale. Ensnared in webs of deceit and trapped in corridors of power and madness, you fought valiantly against the encroaching shadows. As the dungeon's gates close, remember that in every ending, there lies a new beginning. In the annals of Elsinore, your story, though tragic, will be a beacon for those who dare to challenge the status quo."
     }
 
     document.getElementById('result').innerText = resultText;
